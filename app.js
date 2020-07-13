e224ec61e06ca6c6b6ed8d3d0125a4d96cccd201
Emil Tsvetanov <emil.tsvetanov@icloud.com>
2020-07-13 22:11:07 +0300

    Adding a note without duplication Feature Added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Load core modules
-const fs = require('fs')
+// Load Core Modules
 
 // Load Third-Party modules
 const chalk = require('chalk')
@@ -8,7 +7,7 @@ const yargs = require('yargs')
 // Load Own Modules
 const notes = require('./notes.js')
 
-// Create(Add) Note command
+// Add Note Command
 yargs.command({
     command: 'add',
     describe: 'Adding a new note',
@@ -17,14 +16,19 @@ yargs.command({
             describe: 'Title of the note',
             demandOption: true,
             type: 'string'
+        },
+        body: {
+            describe: 'Body of the Note',
+            demandOption: true,
+            type: 'string'
         }
     },
     handler: function(argv) {
-        console.log('New note as added.')
+        notes.addNote(argv.title, argv.body)
     }
 })
 
-// Remove  note
+// Delete Note Command
 yargs.command({
     command: 'remove',
     describe: 'Remove note',
@@ -36,7 +40,32 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('New note as added.')
+        console.log(chalk.red.bold(argv.title) + " note has been deleted.")
+    }
+})
+
+// Read Note Command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Title of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log(chalk.red.bold(argv.title) + " being read")
+    }
+})
+
+// List All Notes Command
+yargs.command({
+    command: 'list',
+    describe: 'List all Notes',
+    handler: function(argv) {
+        console.log('Listing all notes')
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,7 +1,72 @@
+// Load core modules
+const fs = require('fs')
+
 const getNotes = function() {
     return('Your notes...')
 }
 
+// Add note
+const  addNote = function(title, body) {
+    const notes = loadNotes()
+
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })   
+        saveNotes(notes)
+        console.log('New Note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+
+   
+
+    console.log(notes)
+}
+
+// Load All Notes from JSON file
+const loadNotes = function() {
+
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return [] //same if there is no file
+    }
+
+    
+}
+
+const saveNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+// Remove note by title
+const removeNote = function(title) {
+
+}
+
+// Read a note by title
+const readNote = function(title) {
+
+}
+
+// List all notes
+const  listNotes = function() {
+
+}
+
 module.exports = {
-    getNotes: getNotes
+    getNotes: getNotes,
+    loadNotes: loadNotes,
+    addNote: addNote,
+    readNote: readNote,
+    listNotes: listNotes
 }
